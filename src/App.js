cd24cf3429343b554d3627a4df602b5935b1e2ce
darwinmorales <darwin.morales.tech@gmail.com>
2018-03-30 19:11:58 +1100

    add info

--- src/App.js
+++ src/App.js
@@ -22,48 +22,71 @@ class App extends Component {
             </p>
 
             <p>
-              Having received the call in to be “the salt of the earth and light of the world,” we have come to be known
-              as God’s Light. Symbolised by the “bridge” that has become an icon for Sydney, God’s Light sees itself as
-              being a B-R-I-D-G-E.
+              <a href="/who-are-we">Read more...</a>
             </p>
 
-            <p>
-              We acknowledged that we were Broken individuals, Restored by Christ, and now living In the Spirit as
-              Disciples with
-              God being at the centre of our lives. As such, we are called to be Evangelists - witnesses to God’s work
-              in our individual lives. Together we shall bring God’s Light to others.
-            </p>
+            {/*<p>*/}
+              {/*Having received the call in to be “the salt of the earth and light of the world,” we have come to be known*/}
+              {/*as God’s Light. Symbolised by the “bridge” that has become an icon for Sydney, God’s Light sees itself as*/}
+              {/*being a B-R-I-D-G-E.*/}
+            {/*</p>*/}
+
+            {/*<p>*/}
+              {/*We acknowledged that we were Broken individuals, Restored by Christ, and now living In the Spirit as*/}
+              {/*Disciples with*/}
+              {/*God being at the centre of our lives. As such, we are called to be Evangelists - witnesses to God’s work*/}
+              {/*in our individual lives. Together we shall bring God’s Light to others.*/}
+            {/*</p>*/}
           </header>
 
           <header class="panel2"><h2>Our Covenant</h2>
 
             <p>In Christ, God has made a covenant with God's Light, a covenant which God's Light members have joyfully
-              received
-              and entered into by faith and baptism. He has called God's Light members to express in a special way this
-              covenant established by His Son, Jesus, through His sacrifice on the cross. He has joined the members
-              together in the bond of steadfast love and faithfulness, and has given them a particular call and
-              mission.</p>
+              received and entered into by faith and baptism. </p>
 
-            <p>In response, God's Light members desire:</p>
+            <p>
+              <a href="/covenant">Read more...</a>
+            </p>
 
-            <ul>
-              <li>To love and serve Him in lives of daily prayer and service;</li>
-              <li>To praise and worship Him always;</li>
-              <li>To ever seek His face;</li>
-              <li>To know and serve the truth of His Word in the joy, peace, and love of the Holy Spirit;</li>
-              <li>To believe what He speaks to God's Light and to be obedient to the truth of his Word and the guidance
-                of
-                His Spirit;
-              </li>
-              <li>To offer hospitality to those whom He sends to God's Light;</li>
-              <li>To widen our hearts to receive those he adds to God's Light, and</li>
-              <li>To carry out the mission that He is entrusting to God's Light.</li>
-            </ul>
+            {/*<p>*/}
+              {/*He has called God's Light members to express in a special way this*/}
+              {/*covenant established by His Son, Jesus, through His sacrifice on the cross. He has joined the members*/}
+              {/*together in the bond of steadfast love and faithfulness, and has given them a particular call and*/}
+              {/*mission.*/}
+              {/*</p>*/}
+            {/*<p>In response, God's Light members desire:</p>*/}
+
+            {/*<ul>*/}
+              {/*<li>To love and serve Him in lives of daily prayer and service;</li>*/}
+              {/*<li>To praise and worship Him always;</li>*/}
+              {/*<li>To ever seek His face;</li>*/}
+              {/*<li>To know and serve the truth of His Word in the joy, peace, and love of the Holy Spirit;</li>*/}
+              {/*<li>To believe what He speaks to God's Light and to be obedient to the truth of his Word and the guidance*/}
+                {/*of*/}
+                {/*His Spirit;*/}
+              {/*</li>*/}
+              {/*<li>To offer hospitality to those whom He sends to God's Light;</li>*/}
+              {/*<li>To widen our hearts to receive those he adds to God's Light, and</li>*/}
+              {/*<li>To carry out the mission that He is entrusting to God's Light.</li>*/}
+            {/*</ul>*/}
           </header>
 
-          <header class="panel3"><h2> Announcements</h2></header>
-          <header class="panel4"><h2> Events </h2></header>
-          <header class="panel5"><h2> Contact Us </h2></header>
+          <header class="panel3"><h2> Announcements</h2>
+            <p>
+             Easter celebration would be this Sunday 1st April at 11:30am at Epping.
+            </p>
+          </header>
+          <header class="panel4"><h2> Events </h2>
+
+            <ul>
+            <li>1st April - Easter Celebration</li>
+            <li>6th/7th April - Bible Timeline</li>
+            </ul>
+
+          </header>
+          <header class="panel5"><h2> Contact Us </h2>
+            Please email <a href="mailto:john@smith.ee?Subject=Challenge yourself">Robert Jaworski</a> for more info:
+          </header>
 
 
         </div>
